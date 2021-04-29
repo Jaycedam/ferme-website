d340ab68d3758ebe8d5e6b1ca2eb14f2139eaca2
Jordan <jaycedam@gmail.com>
2021-04-29 11:40:31 -0400

    Added Marca model to Filterset

--- app/filters.py
+++ app/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 from django_filters import CharFilter, NumberFilter, ModelChoiceFilter 
-from .models import Producto, TipoProducto
+from .models import Producto, TipoProducto, Marca
 from django import forms
 
 
@@ -8,8 +8,9 @@ class ProductoFilter(django_filters.FilterSet):
     producto = CharFilter(field_name='producto', lookup_expr='icontains', label='Nombre producto')
     precio__gte = NumberFilter(field_name='precio', lookup_expr='gte', label='Precio mínimo')
     precio__lte = NumberFilter(field_name='precio', lookup_expr='lte', label='Precio máximo')
-    id_tipo_producto = ModelChoiceFilter(field_name='id_tipo_producto', label='Tipo producto', queryset=TipoProducto.objects.all())
+    id_tipo_producto = ModelChoiceFilter(empty_label='Todos los tipos', field_name='id_tipo_producto', label='Tipo producto', queryset=TipoProducto.objects.all())
+    id_marca = ModelChoiceFilter(empty_label='Todas las marcas', field_name='id_marca', label='Marca', queryset=Marca.objects.all())
 
     class Meta:
         model = Producto
-        fields = ['producto']
+        fields = ['producto', 'id_tipo_producto', 'id_marca']
